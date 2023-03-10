2756af61d09a503d23e71f47502146b4b38e577a
MaisamMohammadi <mohammadi.m19@htlwienwest.at>
2023-03-10 08:33:05 +0100

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mx-auto text-center">
+    <NavbarComp></NavbarComp>
+    <RouterView />
+  </div>
+</template>
+<script setup>
+import NavbarComp from '@/components/NavbarComp.vue';
+import { RouterView } from 'vue-router';
+</script>
---
+++ frontend/src/components/NavbarComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mt-8 mb-12">
+    <nav class="text-xl">
+      <RouterLink
+        to="/"
+        class="decoration-emerald-30 mr-5 rounded-xl py-1 px-5 decoration-emerald-300 underline-offset-8 hover:underline focus:underline active:bg-emerald-50"
+        >Home
+      </RouterLink>
+      <RouterLink
+        to="/about"
+        class="decoration-emerald-30 mr-5 rounded-xl py-1 px-5 decoration-emerald-300 underline-offset-8 hover:underline focus:underline active:bg-emerald-50"
+      >
+        About
+      </RouterLink>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
