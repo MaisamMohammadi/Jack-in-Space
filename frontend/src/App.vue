09f6d1908f43001e616264470f4e4f2a8ba7e505
MaisamMohammadi <mohammadi.m19@htlwienwest.at>
2023-03-15 14:09:52 +0100

    Fortsetzung
    Signed-off-by: MaisamMohammadi <mohammadi.m19@htlwienwest.at>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="container mx-auto text-center">
+  <div class="container mx-auto text-center window">
     <NavbarComp></NavbarComp>
     <RouterView />
+    <img src="./Spaceship_Model.png" alt="" :class="spaceship ">
   </div>
 </template>
 <script setup>
+import { ref } from 'vue' 
 import NavbarComp from '@/components/NavbarComp.vue';
 import { RouterView } from 'vue-router';
+
+const left = ref(50);
+const right = ref(50);
+
 </script>
+<style>
+.window{
+  cursor: url(https://www.svgrepo.com/show/488688/space-shuttle.svg), auto !important;
+}
+.spaceship{
+  width: 100px;
+  height: 100px;
+  left: 50px;
+  top: 50px;
+}
+</style>
