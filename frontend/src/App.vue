d0cc7370ddd579a2e09cdf128c1284132d06719c
MaisamMohammadi <mohammadi.m19@htlwienwest.at>
2023-03-22 11:47:13 +0100

    Spaceship changes
    Signed-off-by: MaisamMohammadi <mohammadi.m19@htlwienwest.at>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,9 +4,9 @@
   </div>
 </template>
 <script setup>
-import { ref, computed } from 'vue' 
-import NavbarComp from '@/components/NavbarComp.vue';
-import { RouterView } from 'vue-router';
+// import { ref, computed } from 'vue' 
+// import NavbarComp from '@/components/NavbarComp.vue';
+// import { RouterView } from 'vue-router';
 import GameView from '@/views/GameView.vue';
 
 </script>
--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -4,8 +4,8 @@
     </div>
 </template>
 <script setup>
-import { ref, computed } from 'vue' 
-import { RouterView } from 'vue-router';
+// import { ref, computed } from 'vue' 
+// import { RouterView } from 'vue-router';
 import Phaser from 'phaser';
 import spaceship from '@/assets/images/spaceship_Modell.png';
 import trash from '@/assets/images/trash_Modell.png';
@@ -18,18 +18,22 @@ class gameScene extends Phaser.Scene{
 
     preload(){
         this.load.image('trash', trash);
-        this.load.image('ship', spaceship);
+        this.load.image('spaceship', spaceship);
     }
 
     create(){
-        this.add.image(100, 200, 'ship');
+        this.add.image(100, 200, 'spaceship');
         this.add.image(50, 50, 'trash');
+    }
+
+    movetrash(trash, speed){
+        trash.y = speed;
     }
 } 
 
 const config = {
     width: 800,
-    height: 800,
+    height: 600,
     backgroundColor: 0x000000,
     scene: [gameScene]
 }
