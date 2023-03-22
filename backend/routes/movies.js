25da23bcbaeba951e340b0b4e2e594e04b0ae678
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2023-03-22 12:07:20 +0100

    Format existing JS files to satisfy ESLint

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from 'express'
 // Here you import your functions from controllers
-import { getMovies, getMovie } from '../controllers/movies.js';
+import { getMovies, getMovie } from '../controllers/movies.js'
 
-const router = express.Router();
+const router = express.Router()
 
 // Here come your routes
-router.get('/', getMovies);
-router.get('/:id', getMovie);
+router.get('/', getMovies)
+router.get('/:id', getMovie)
 
-export default router;
+export default router
