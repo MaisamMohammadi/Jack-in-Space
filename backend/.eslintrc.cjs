603572e48c11b637b7ad10cf4fc89f99a5a029e7
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2023-03-22 11:56:53 +0100

    Use JavaScript Standard Style instead of Airbnb

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,17 +1,17 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: ['airbnb-base'],
+  extends: ['standard'],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
     'linebreak-style': 0,
     'no-console': 0,
-    'import/extensions': 'off',
-    'object-curly-newline': 'never',
-  },
-};
+    'import/extensions': 0,
+    'object-curly-newline': 0
+  }
+}
