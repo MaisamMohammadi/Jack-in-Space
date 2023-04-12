b77ab606907ae0ccd2d0907b23629c704bb53eec
MaisamMohammadi <mohammadi.m19@htlwienwest.at>
2023-04-12 21:39:20 +0200

    It changed
    Signed-off-by: MaisamMohammadi <mohammadi.m19@htlwienwest.at>

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import GameView from '../views/GameView.vue';
-import LoginView from '../views/LoginView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="w-[50vw] h-[50vh] bg-blue absolute left-[25vw] top-[25vh] px-auto">
     <p class="text-center text-[50px]">Jack in Space</p>
-    <div class="text-center text-xl mt-[5vh]">
-      <router-link v-for="item in menuItems" :key="item" :to="{ name: item.link }"><div>{{item.title}}</div></router-link>
+    <div class="flex flex-col items-center text-xl my-[25px]">
+      <router-link v-for="item in menuItems" :key="item" :to="{ name: item.link }" class="my-[5px]">
+        <div class="w-[250px] bg-yellow px-1 py-1 text-center">{{item.title}}</div>
+      </router-link>
     </div>
+
   </div>
 </template>
 
