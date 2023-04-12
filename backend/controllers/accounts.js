b2f98c196bc96ce618b4a66bd6daa376d7696b63
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2023-04-12 12:30:08 +0200

    Revamp stuff

--- backend/controllers/accounts.js
+++ backend/controllers/accounts.js
@@ -1,42 +1,78 @@
 // import { UserValidate } from '../validators/validators.js'
 import path from 'path'
 import fs from 'fs'
+import bcrypt from 'bcryptjs'
 
+const salt = bcrypt.genSaltSync(10)
 const dirname = path.resolve()
 const data = JSON.parse(fs.readFileSync(path.join(dirname, './models/data.json')))
 const users = data.users
 
+const verifyAccount = (username, password) => {
+  const user = users.find((user) => user.username === username)
+  // password = bcrypt.hashSync(password, salt)
+  if (!user) return { username: false, password: false }
+  // if (user.password !== password) return { username: true, password: false }
+  if (!bcrypt.compareSync(password, user.password)) return { username: true, password: false }
+  return { username: true, password: true }
+}
+
 const fetchAccounts = (req, res) => {
-  const { username, password } = req.query
+  const { username, password } = req.body
   if (!username || !password) {
     res.status(200).json(users)
   } else {
-    const user = users.find((user) => user.username === username)
-    if (!user) {
+    const accountVerificationResult = verifyAccount(username, password)
+    if (!accountVerificationResult.username && !accountVerificationResult.password) {
       res.status(404).json({
         message:
           'Could not log in because the user was not found. If you typed your username correctly, then you may not have an account yet. Please register by clicking the "register" button.'
       })
-      return
-    }
-    if (user.password !== password) {
+    } else if (accountVerificationResult.username && !accountVerificationResult.password) {
       res.status(401).json({
         message:
           'Could not log in because given password is incorrect. Please make sure you typed your password correctly, and then try again.'
       })
-      return
+    } else if (accountVerificationResult.username && accountVerificationResult.password) {
+      res.status(200).json(users.find((user) => user.username === username))
+    } else {
+      res.status(500).json({
+        message: 'An unexpected error occurred while trying to log in. Please try again later.'
+      })
     }
-    res.status(200).json(user)
+    // switch (accountVerificationResult) {
+    //   case { username: false, password: false }:
+    //     res.status(404).json({
+    //       message:
+    //         'Could not log in because the user was not found. If you typed your username correctly, then you may not have an account yet. Please register by clicking the "register" button.'
+    //     })
+    //     break
+    //   case { username: true, password: false }:
+    //     res.status(401).json({
+    //       message:
+    //         'Could not log in because given password is incorrect. Please make sure you typed your password correctly, and then try again.'
+    //     })
+    //     break
+    //   case { username: true, password: true }:
+    //     res.status(200).json(users.find((user) => user.username === username))
+    //     break
+    //   default:
+    //     res.status(500).json({
+    //       message: 'An unexpected error occurred while trying to log in. Please try again later.'
+    //     })
+    //     break
+    // }
   }
 }
 
 const addAccount = (req, res) => {
-  const { username, password, age } = req.body
+  const { username, password, birthdate } = req.body
+  const age = new Date().getFullYear() - new Date(birthdate).getFullYear()
   const user = users.find((user) => user.username === username)
-  if (!username || !password || !age) {
+  if (!username || !password || !birthdate) {
     res.status(400).json({
       message:
-        'Could not add account because one or more required fields were not provided. Please make sure you provided a username, password, and age.'
+        'Could not add account because one or more required fields were not provided. Please make sure you provided a username, password, and your birthdate.'
     })
     return
   }
@@ -57,7 +93,8 @@ const addAccount = (req, res) => {
   const newUser = {
     id: users.length + 1,
     username,
-    password,
+    password: bcrypt.hashSync(password, salt),
+    birthdate,
     age,
     highscore: 0,
     coins: 0,
