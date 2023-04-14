be719eef6f1a90981fb492175e1a1ae344123d7a
MaisamMohammadi <mohammadi.m19@htlwienwest.at>
2023-04-14 21:58:20 +0200

    changed

--- frontend/src/views/HighscoreView.vue
+++ frontend/src/views/HighscoreView.vue
@@ -1,13 +1,43 @@
 <template>
-    <div>
-
-    </div>
+  <div class="absolute left-[35vw] top-[25vh]">
+    <router-link :to="{ name: 'Home' }" class="px-[10px] py-[10px] bg-yellow"><span class="my-[10px]">back</span></router-link>
+    <table class="w-[30vw] mt-[30px] table-auto">
+      <thead class="bg-blue">
+        <tr class="">
+          <th class="py-3">rank</th>
+          <th class="py-3">name</th>
+          <th class="py-3">ponits</th>
+        </tr>
+      </thead>
+      <tbody class="bg-yellow">
+        <tr v-for="el in rankingList" :key="el">
+          <td class="px-3 py-3 text-center">
+            {{ rankingList.indexOf(el) + 1 }}
+          </td>
+          <td class="px-3 py-3 text-center">{{ el.name }}</td>
+          <td class="px-3 py-3 text-center">{{ el.ponits }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
 
+const rankingList = [
+  {
+    name: "Maisam",
+    ponits: 234,
+  },
+  {
+    name: "Rana",
+    ponits: 200,
+  },
+  {
+    name: "Maisam",
+    ponits: 150,
+  },
+];
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
