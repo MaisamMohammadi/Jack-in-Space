443d80fb016cd5634b5e7e657435a7d1dcc042cc
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2023-04-26 00:25:05 +0200

    some fixing

--- backend/src/api/controllers/accounts.js
+++ backend/src/api/controllers/accounts.js
@@ -29,12 +29,20 @@ const fetchAccount = async (req, res) => {
 const authenticateAccount = async (idOrUsername, password) => {
   const user = await model.dbFetchAccount(idOrUsername)
   if (!user) return { username: false, password: false }
-  if (!bcrypt.compareSync(password, user.password)) return { username: true, password: false }
+  if (!bcrypt.compareSync(password, user.password)) {
+    return { username: true, password: false }
+  }
   return { username: true, password: true }
 }
 
 const authenticateAccountRoute = async (req, res) => {
   const { username, password } = req.body
+  if (isNullOrWhitespace(username) || isNullOrWhitespace(password)) {
+    res
+      .status(400)
+      .json({ message: 'Supplied data (username or password) is empty' })
+    return
+  }
   const result = await authenticateAccount(username, password)
   if (result.username && result.password) {
     res.status(200).json({ message: 'Authentication successful' })
@@ -51,14 +59,36 @@ const addAccount = async (req, res) => {
     res.status(409).json({ message: 'Username already taken' })
     return
   }
+  if (
+    isNullOrWhitespace(username) ||
+    isNullOrWhitespace(password) ||
+    isNullOrUndefined(birthdate)
+  ) {
+    res.status(400).json({
+      message: 'Supplied data (username, password or birthdate) is empty'
+    })
+    return
+  }
   const salt = await bcrypt.genSalt(10)
   const hashedPassword = await bcrypt.hash(password, salt)
   const account = await model.dbAddAccount(username, hashedPassword, birthdate)
+  if (!account) {
+    res.status(400).json({
+      message:
+        'Sorry, you are too young. You must be at least 8 years old or older.'
+    })
+    return
+  }
   res.status(201).json(account)
 }
 
 const updateAccount = async (req, res) => {
   const { id, method } = req.params
+  const account = await model.dbFetchAccount(id)
+  if (!account) {
+    res.status(404).json({ message: 'Account not found' })
+    return
+  }
   let result
   switch (method) {
     case 'username':
@@ -95,7 +125,11 @@ const updateAccount = async (req, res) => {
 const updateAccountUsername = async (id, requestBody) => {
   const { username } = requestBody
   if (isNullOrWhitespace(username)) {
-    return { jackIsLost: true, status: 400, message: 'Supplied data (username) is empty' }
+    return {
+      jackIsLost: true,
+      status: 400,
+      message: 'Supplied data (username) is empty'
+    }
   }
   if (await checkIfUsernameTaken(username)) {
     return { jackIsLost: true, status: 409, message: 'Username already taken' }
@@ -129,19 +163,39 @@ const updateAccountPassword = async (id, requestBody) => {
 const updateAccountBirthdate = async (id, requestBody) => {
   const { birthdate } = requestBody
   if (isNullOrWhitespace(birthdate)) {
-    return { jackIsLost: true, status: 400, message: 'Supplied data (birthdate) is empty' }
+    return {
+      jackIsLost: true,
+      status: 400,
+      message: 'Supplied data (birthdate) is empty'
+    }
   }
   const account = await model.dbUpdateAccountBirthdate(id, birthdate)
+  if (!account) {
+    return {
+      jackIsLost: true,
+      status: 400,
+      message:
+        'Sorry, you are too young. You must be at least 8 years old or older.'
+    }
+  }
   return account
 }
 
 const updateAccountHighscore = async (id, requestBody) => {
   const { highscore } = requestBody
   if (isNullOrUndefined(highscore)) {
-    return { jackIsLost: true, status: 400, message: 'Supplied data (highscore) is empty' }
+    return {
+      jackIsLost: true,
+      status: 400,
+      message: 'Supplied data (highscore) is empty'
+    }
   }
   if (isNaN(Number(highscore))) {
-    return { jackIsLost: true, status: 400, message: 'Supplied data (highscore) is not a number' }
+    return {
+      jackIsLost: true,
+      status: 400,
+      message: 'Supplied data (highscore) is not a number'
+    }
   }
   const account = await model.dbUpdateAccountHighscore(id, highscore)
   return account
@@ -150,10 +204,18 @@ const updateAccountHighscore = async (id, requestBody) => {
 const updateAccountCoins = async (id, requestBody) => {
   const { coins } = requestBody
   if (isNullOrUndefined(coins)) {
-    return { jackIsLost: true, status: 400, message: 'Supplied data (coins) is empty' }
+    return {
+      jackIsLost: true,
+      status: 400,
+      message: 'Supplied data (coins) is empty'
+    }
   }
   if (isNaN(Number(coins))) {
-    return { jackIsLost: true, status: 400, message: 'Supplied data (coins) is not a number' }
+    return {
+      jackIsLost: true,
+      status: 400,
+      message: 'Supplied data (coins) is not a number'
+    }
   }
   const account = await model.dbUpdateAccountCoins(id, coins)
   return account
@@ -162,10 +224,18 @@ const updateAccountCoins = async (id, requestBody) => {
 const updateAccountSkinShip = async (id, requestBody) => {
   let { skinShip } = requestBody
   if (isNullOrWhitespace(skinShip)) {
-    return { jackIsLost: true, status: 400, message: 'Supplied data (skinShip) is empty' }
+    return {
+      jackIsLost: true,
+      status: 400,
+      message: 'Supplied data (skinShip) is empty'
+    }
   }
   if (isNaN(Number(skinShip))) {
-    return { jackIsLost: true, status: 400, message: 'Supplied data (skinShip) is not a number' }
+    return {
+      jackIsLost: true,
+      status: 400,
+      message: 'Supplied data (skinShip) is not a number'
+    }
   }
   skinShip = Number(skinShip)
   const account = await model.dbUpdateAccountSkinShip(id, skinShip)
@@ -175,10 +245,18 @@ const updateAccountSkinShip = async (id, requestBody) => {
 const updateAccountSkinLaser = async (id, requestBody) => {
   const { skinLaser } = requestBody
   if (isNullOrWhitespace(skinLaser)) {
-    return { jackIsLost: true, status: 400, message: 'Supplied data (skinLaser) is empty' }
+    return {
+      jackIsLost: true,
+      status: 400,
+      message: 'Supplied data (skinLaser) is empty'
+    }
   }
   if (isNaN(Number(skinLaser))) {
-    return { jackIsLost: true, status: 400, message: 'Supplied data (skinLaser) is not a number' }
+    return {
+      jackIsLost: true,
+      status: 400,
+      message: 'Supplied data (skinLaser) is not a number'
+    }
   }
   const account = await model.dbUpdateAccountSkinLaser(id, skinLaser)
   return account
@@ -186,8 +264,13 @@ const updateAccountSkinLaser = async (id, requestBody) => {
 
 const deleteAccount = async (req, res) => {
   const { id } = req.params
-  const account = await model.dbDeleteAccount(id)
-  res.status(200).json(account)
+  const account = await model.dbFetchAccount(id)
+  if (!account) {
+    res.status(404).json({ message: 'Account not found' })
+    return
+  }
+  const deletedAccount = await model.dbDeleteAccount(id)
+  res.status(200).json(deletedAccount)
 }
 
 export {
