140eaba444b2a7cc36b170f0bead28a37506c1e2
MaisamMohammadi <mohammadi.m19@htlwienwest.at>
2023-04-25 21:20:42 +0200

    changed

--- frontend/src/views/HelpView.vue
+++ frontend/src/views/HelpView.vue
@@ -1,19 +1,26 @@
 <template>
-    <div class="w-screen h-screen view">
-
+  <div class="view h-screen w-screen">
+    <div id="container" class="flex">
+      <div>
+        <p>Mit Maus, kannst du den Spaceship bewegen</p>
+        <p>Mit Rechtsclick, kannst du Laser feuern.</p>
+        <p>Du muss die Mülls, die fallen, abschießen.</p>
+      </div>
+      <div>
+        <img src="@/assets/images/Help.svg" class="w-[400px]" alt="" />
+      </div>
     </div>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped>
-.view{
-  background-image: url('../assets/images/background.png');
+.view {
+  background-image: url("../assets/images/background.png");
   background-size: cover;
 }
-p{
-  font-family: title;
+p {
+  font-family: overwatch;
 }
 </style>
