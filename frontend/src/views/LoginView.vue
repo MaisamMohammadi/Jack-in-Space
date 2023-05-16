b351c4cdff272e10b16d710517244fb5c0034881
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2023-05-17 00:41:48 +0200

    Formatting idk

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,72 +1,75 @@
 <template>
   <div class="w-screen h-screen view">
-    <div id="container" class="w-[40vw] h-[40vw] flex flex-col justify-center items-center bg-circle/50 absolute left-[30vw] top-[12vh] px-auto rounded-full border-[1vw] border-blue">
-          <p class="text-center text-[100px] mt-[5%]">Login</p>
+    <div id="container"
+      class="px-auto absolute left-[30vw] top-[12vh] flex h-[40vw] w-[40vw] flex-col items-center justify-center rounded-full border-[1vw] border-blue bg-circle/50">
+      <p class="mt-[5%] text-center text-[100px]">Login</p>
 
-          <div class="mt-10">
-            <div class="sm:col-span-3">
-              <label for="user-name" class="text-lg font-medium leading-6">User name</label>
-              <div class="mt-2">
-                <input type="text" name="user-name" id="user-name" v-model="data.username" autocomplete="given-name" class="block w-[300px] h-[40px] rounded-md border-0 px-1 py-1.5 text-black shadow-sm bg-white focus:bg-white/50" />
-              </div>
-            </div>
+      <div class="mt-10">
+        <div class="sm:col-span-3">
+          <label for="user-name" class="text-lg font-medium leading-6">User name</label>
+          <div class="mt-2">
+            <input type="text" name="user-name" id="user-name" v-model="data.username" autocomplete="given-name"
+              class="block h-[40px] w-[300px] rounded-md border-0 bg-white px-1 py-1.5 text-black shadow-sm focus:bg-white/50" />
+          </div>
+        </div>
 
-            <!-- <div class="sm:col-span-4">
+        <!-- <div class="sm:col-span-4">
               <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
               <div class="mt-2">
                 <input id="email" name="email" type="email" v-model="email" autocomplete="email" class="block w-[500px] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
               </div>
             </div> -->
 
-            <div class="mt-5">
-              <label for="password" class="block text-lg font-medium leading-6">Password</label>
-              <div class="mt-2">
-                <input id="password" name="password" type="password" v-model="data.password" autocomplete="password" class="block w-[300px] h-[40px] rounded-md border-0 px-1 py-1.5 text-black shadow-sm bg-white focus:bg-white/50" />
-              </div>
-            </div>
-
+        <div class="mt-5">
+          <label for="password" class="block text-lg font-medium leading-6">Password</label>
+          <div class="mt-2">
+            <input id="password" name="password" type="password" v-model="data.password" autocomplete="password"
+              class="block h-[40px] w-[300px] rounded-md border-0 bg-white px-1 py-1.5 text-black shadow-sm focus:bg-white/50" />
           </div>
-          
-          <router-link :to="{ name: 'Register'}">
-            <div class="w-[15vw] h-[6vh] px-1 py-1 my-[15px] flex justify-center items-center text-[25px]">Register</div>
-          </router-link>
-          
-        
-          <div class="w-[15vw] h-[6vh] px-1 py-1 my-[15px] flex justify-center items-center border-[3px] border-blue text-[35px]" @click="getResult">LOGIN</div>
-          
-        
+        </div>
       </div>
-      <router-link :to="{ name: 'Home' }" class="my-[5px]">
-        <div class="absolute left-[2vw] bottom-[2vw] flex h-[6vh] w-[15vw] items-center justify-center border-[3px] border-red px-1 py-1 text-[35px] text-red">HOME</div>
+
+      <router-link :to="{ name: 'Register' }">
+        <div class="my-[15px] flex h-[6vh] w-[15vw] items-center justify-center px-1 py-1 text-[25px]">
+          Register
+        </div>
       </router-link>
+
+      <div
+        class="my-[15px] flex h-[6vh] w-[15vw] items-center justify-center border-[3px] border-blue px-1 py-1 text-[35px]"
+        @click="getResult">
+        LOGIN
+      </div>
+    </div>
+    <router-link :to="{ name: 'Home' }" class="my-[5px]">
+      <div
+        class="absolute left-[2vw] bottom-[2vw] flex h-[6vh] w-[15vw] items-center justify-center border-[3px] border-red px-1 py-1 text-[35px] text-red">
+        HOME
+      </div>
+    </router-link>
   </div>
-    
-
-    
-
-    
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
 const data = ref({
-  username : '',
-  password : '',
+  username: '',
+  password: ''
 })
 
 const getResult = () => {
-  console.log(data.value);
+  console.log(data.value)
 }
-
 </script>
 
 <style scoped>
-.view{
+.view {
   background-image: url('../assets/images/background.png');
   background-size: cover;
 }
-p{
+
+p {
   font-family: title;
 }
 </style>
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,73 +1,76 @@
 <template>
   <div class="w-screen h-screen view">
-    <div id="container" class="w-[40vw] h-[40vw] flex flex-col justify-center items-center bg-circle/50 absolute left-[30vw] top-[12vh] px-auto rounded-full border-[1vw] border-blue">
-          <p class="text-center text-[100px] mt-[5%]">REGISTER</p>
+    <div id="container"
+      class="w-[40vw] h-[40vw] flex flex-col justify-center items-center bg-circle/50 absolute left-[30vw] top-[12vh] px-auto rounded-full border-[1vw] border-blue">
+      <p class="text-center text-[100px] mt-[5%]">REGISTER</p>
 
-          <div class="mt-10">
-            <div class="sm:col-span-3">
-              <label for="user-name" class="text-lg font-medium leading-6">User name</label>
-              <div class="mt-2">
-                <input type="text" name="user-name" id="user-name" v-model="data.username" autocomplete="given-name" class="block w-[300px] h-[40px] rounded-md border-0 px-1 py-1.5 text-black shadow-sm bg-white focus:bg-white/50" />
-              </div>
-            </div>
-
-            <div class="mt-5">
-              <label for="password" class="block text-lg font-medium leading-6">Password</label>
-              <div class="mt-2">
-                <input id="password" name="password" type="password" v-model="data.password" autocomplete="password" class="block w-[300px] h-[40px] rounded-md border-0 px-1 py-1.5 text-black shadow-sm bg-white focus:bg-white/50" />
-              </div>
-            </div>
-
-            <div class="mt-5">
-              <label for="birthdate" class="block text-lg font-medium leading-6">birthdate</label>
-              <div class="mt-2">
-                <input id="birthdate" name="birthdate" type="date" v-model="data.birthdate" autocomplete="password" class="block w-[300px] h-[40px] rounded-md border-0 px-1 py-1.5 text-black shadow-sm bg-white focus:bg-white/50" />
-              </div>
-            </div>
-
+      <div class="mt-10">
+        <div class="sm:col-span-3">
+          <label for="user-name" class="text-lg font-medium leading-6">User name</label>
+          <div class="mt-2">
+            <input type="text" name="user-name" id="user-name" v-model="data.username" autocomplete="given-name"
+              class="block w-[300px] h-[40px] rounded-md border-0 px-1 py-1.5 text-black shadow-sm bg-white focus:bg-white/50" />
           </div>
-          
-          <router-link :to="{ name: 'Login'}">
-            <div class="w-[15vw] h-[6vh] px-1 py-1 my-[15px] flex justify-center items-center text-[25px]">Login</div>
-          </router-link>
-          
-        
-          <div class="w-[15vw] h-[6vh] px-1 py-1 my-[15px] flex justify-center items-center border-[3px] border-blue text-[35px]" @click="getResult">Register</div>
-          
-        
+        </div>
+
+        <div class="mt-5">
+          <label for="password" class="block text-lg font-medium leading-6">Password</label>
+          <div class="mt-2">
+            <input id="password" name="password" type="password" v-model="data.password" autocomplete="password"
+              class="block w-[300px] h-[40px] rounded-md border-0 px-1 py-1.5 text-black shadow-sm bg-white focus:bg-white/50" />
+          </div>
+        </div>
+
+        <div class="mt-5">
+          <label for="birthdate" class="block text-lg font-medium leading-6">birthdate</label>
+          <div class="mt-2">
+            <input id="birthdate" name="birthdate" type="date" v-model="data.birthdate" autocomplete="password"
+              class="block w-[300px] h-[40px] rounded-md border-0 px-1 py-1.5 text-black shadow-sm bg-white focus:bg-white/50" />
+          </div>
+        </div>
+
       </div>
-      <router-link :to="{ name: 'Home' }" class="my-[5px]">
-        <div class="absolute left-[2vw] bottom-[2vw] flex h-[6vh] w-[15vw] items-center justify-center border-[3px] border-red px-1 py-1 text-[35px] text-red">HOME</div>
+
+      <router-link :to="{ name: 'Login' }">
+        <div class="w-[15vw] h-[6vh] px-1 py-1 my-[15px] flex justify-center items-center text-[25px]">Login</div>
       </router-link>
+
+
+      <div
+        class="w-[15vw] h-[6vh] px-1 py-1 my-[15px] flex justify-center items-center border-[3px] border-blue text-[35px]"
+        @click="getResult">Register</div>
+
+
+    </div>
+    <router-link :to="{ name: 'Home' }" class="my-[5px]">
+      <div
+        class="absolute left-[2vw] bottom-[2vw] flex h-[6vh] w-[15vw] items-center justify-center border-[3px] border-red px-1 py-1 text-[35px] text-red">
+        HOME</div>
+    </router-link>
   </div>
-    
-
-    
-
-    
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
 const data = ref({
-  username : '',
-  password : '',
+  username: '',
+  password: '',
   birthdate: '',
 })
 
 const getResult = () => {
-  console.log(data.value);
+  console.log(data.value)
 }
 
 </script>
 
 <style scoped>
-.view{
+.view {
   background-image: url('../assets/images/background.png');
   background-size: cover;
 }
-p{
+
+p {
   font-family: title;
-}
-</style>
+}</style>
